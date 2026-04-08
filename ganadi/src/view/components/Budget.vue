e61caaf4e9c4d05f3a98ee55deb3c5f7fe016876
GJyeon <junyeon03@gmail.com>
2026-04-08 10:13:15 +0900

    feat: 목표지출 설정 ui 구현

---
+++ ganadi/src/view/components/Budget.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="set-group">
+        <h2>{{ targetMonth }}월 목표지출 설정</h2>
+    </div>
+    <div class="set-group">
+        <!-- 숫자에 , 찍기 위해 -->
+        <input type="number" pattern="[0-9]*" min="0" id="target-amount" placeholder="금액을 입력하세요"> 
+        <button type="button">수정</button>
+    </div>
+</template>
+
+<script setup>
+    const targetMonth = ref(''); // 이번 달
+    const goalAmount = ref('');
+</script>
+
+<style scoped>
+
+</style>
