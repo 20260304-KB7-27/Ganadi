28fcafc44a8db3344c6215fbb8a54054d0e08beb
Kimyerim <yeye092302@gmail.com>
2026-04-08 15:11:27 +0900

    feat: 목업 데이터 파일 생성

---
+++ ganadi/src/model/mock/graphMockData.js
@@ -0,0 +1,24 @@
+export const graphMockData = {
+    totalExpense: 37000,
+    monthlyGoal: 500000,
+    goalRate: 7,
+    characterLevel: 1,
+    categorySummary: [
+        {
+            categoryId: '2',
+            name: '식비',
+            icon: 'fare',
+            color: '#33A1FF',
+            amount: 24000,
+            percent: 65,
+        },
+        {
+            categoryId: '3',
+            name: '병원',
+            icon: 'cafe',
+            color: '#FF5733',
+            amount: 13000,
+            percent: 35,
+        },
+    ],
+};
