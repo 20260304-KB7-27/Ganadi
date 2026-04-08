ee4cf4b6b3d9e48770bcc729580c59091ee09233
Kimyerim <yeye092302@gmail.com>
2026-04-08 15:11:55 +0900

    fix: 오타 수정

--- ganadi/src/router/index.js
+++ ganadi/src/router/index.js
@@ -2,16 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const Home = () => import('@/view/pages/Home.vue');
 const Graph = () => import('@/view/pages/Graph.vue');
-const Setting = () => import('@/view/pages/Setting.vue');-
-
+const Setting = () => import('@/view/pages/Setting.vue');
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', name: 'Home', component: Home },
-    { path: '/graph', name: 'About', component: Graph },
-    { path: '/setting', name: 'Setting', component: Setting },
-
-  ],
+    history: createWebHistory(),
+    routes: [
+        { path: '/', name: 'Home', component: Home },
+        { path: '/graph', name: 'About', component: Graph },
+        { path: '/setting', name: 'Setting', component: Setting },
+    ],
 });
 export default router;
