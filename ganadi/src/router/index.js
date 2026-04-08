f9cbcf60bb97d84e4e2d5ef7acc1be8c2aa9bb77
김호성 <000731khs@naver.com>
2026-04-08 09:46:19 +0900

    fix/index js 오탈자 제거

--- ganadi/src/router/index.js
+++ ganadi/src/router/index.js
@@ -2,8 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const Home = () => import('@/view/pages/Home.vue');
 const Graph = () => import('@/view/pages/Graph.vue');
-const Setting = () => import('@/view/pages/Setting.vue');-
-
+const Setting = () => import('@/view/pages/Setting.vue');
 
 const router = createRouter({
   history: createWebHistory(),
@@ -11,7 +10,6 @@ const router = createRouter({
     { path: '/', name: 'Home', component: Home },
     { path: '/graph', name: 'About', component: Graph },
     { path: '/setting', name: 'Setting', component: Setting },
-
   ],
 });
 export default router;
