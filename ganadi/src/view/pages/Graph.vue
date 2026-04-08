e675cfffd02dc68d24770a6f33a31dc43524d1d1
yerimming <120501910+yerimming@users.noreply.github.com>
2026-04-08 17:00:53 +0900

    Feature/graph (#14)
    * chore: 캐릭터 레벨 이미지 추가
    
    * feat: 목업 데이터 파일 생성
    
    * fix: 오타 수정
    
    * feat: mock 데이터에 맞춘 통계  페이지 구성
    
    * feat: 도넛 차트 생성
    
    * style: 캐릭터 위치 조정
    
    * refactor: 사용하지 않는 goal rate prop 제거
    
    * style: icon-box에서 font-size 삭제 후 right-area 속성 추가
    
    * fix: Graph.vue와 graphChart.vue props 이름 불일치 문제 수정
    
    * style: 통계 리스트 스타일 지정
    
    * style: bootstrap icon 사용해 카테고리 보이도록 수정
    
    * style: 도넛 차트 두께 조정, 마우스 hover 효과 삭제
    
    * readme 수정
    
    ---------
    
    Co-authored-by: 김호성 <000731khs@naver.com>

--- ganadi/src/view/pages/Graph.vue
+++ ganadi/src/view/pages/Graph.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="card card -body">
-    <h2>Graph</h2>
-  </div>
+    <div class="graph-page">
+        <GraphHeader :total-expense="graphData.totalExpense" />
+        <GraphChart
+            :items="graphData.categorySummary"
+            :level="graphData.characterLevel"
+        />
+        <GraphCategoryList :items="graphData.categorySummary" />
+    </div>
 </template>
-<script>
-export default {
-  name: 'Graph',
-};
+
+<script setup>
+import GraphHeader from '../components/graph/graphHeader.vue';
+import GraphCategoryList from '../components/graph/graphCategoryList.vue';
+import GraphChart from '../components/graph/graphChart.vue';
+import { graphMockData } from '@/model/mock/graphMockData';
+
+const graphData = graphMockData;
 </script>
+
+<style scoped>
+.graph-page {
+    min-height: 100vh;
+    padding: 16px 16px 90px;
+    display: flex;
+    flex-direction: column;
+}
+</style>
