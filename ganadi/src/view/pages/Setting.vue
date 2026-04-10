e7d6931b8e87a6efe718fb25d0bee6b0a823d9f3
Kimyerim <yeye092302@gmail.com>
2026-04-10 09:21:51 +0900

    style: 전역 스타일 scoped 적용

--- ganadi/src/view/pages/Setting.vue
+++ ganadi/src/view/pages/Setting.vue
@@ -1,20 +1,21 @@
 <template>
-  <div class="card">
-    <Budget />
-    <SetDefault />
-    <SetCategory />
-  </div>
+    <div class="card">
+        <Budget />
+        <SetDefault />
+        <SetCategory />
+    </div>
 </template>
 <script setup>
 import Budget from '../components/Budget.vue';
 import SetCategory from '../components/SetCategory.vue';
 import SetDefault from '../components/SetDefault.vue';
 </script>
-<style>
+
+<style scoped>
 .card {
-  background: #f5f5f5;
-  padding: 20px;
-  border-radius: 16px;
-  /* width: 320px; */
+    background: #f5f5f5;
+    padding: 20px;
+    border-radius: 16px;
+    /* width: 320px; */
 }
 </style>
