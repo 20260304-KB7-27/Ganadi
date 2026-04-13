b65e16fbf9d2cb82e419ac8f8ade529523efd292
moonsuho <112683632+moonsuho@users.noreply.github.com>
2026-04-13 13:23:49 +0900

    feat: PWA를 위한 코드 추가

--- ganadi/vite.config.js
+++ ganadi/vite.config.js
@@ -1,15 +1,25 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: "autoUpdate",
+      filename: "sw.js",
+      devOptions: { enabled: true, type: "module" },
+      injectRegister: "auto",
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
       // '@model': fileURLToPath(new URL('./src/model', import.meta.url)),
       // '@viewmodel': fileURLToPath(new URL('./src/viewmodel', import.meta.url)),
       // '@view': fileURLToPath(new URL('./src/view', import.meta.url)),
@@ -18,10 +28,10 @@ export default defineConfig({
 
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
